655d19ccbcf7f766237015b066f79b85879b462d
Lokendra Tyagi <lokendra.tyagi@egovernments.org>
2025-03-05 16:50:01 +0530

    update json file

--- exec.py
+++ exec.py
@@ -1,7 +1,7 @@
 from migrate_schema import *
 from migrate_data import *
 
-create_schema("schema/naljal.json", tenantId="as")
+create_schema("schema/BPA.json", tenantId="pg")
 # create_all_schema("schema", tenantId="as")
 # create_all_schema("schema", tenantId="as", is_portforward=False)
 
